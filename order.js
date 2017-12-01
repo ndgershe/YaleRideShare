efa869797255b0a131b13a7294a338fd192f8cf8
Jessica McCurdy <jessica.mccurdy@yale.edu>
2017-12-01 02:41:18 +0000

    Add order.js

---
+++ order.js
@@ -0,0 +1,18 @@
+$("#myselect").on("change", function() {
+    $("#" + $(this).val()).show().siblings();
+})
+
+let airport = document.airport.value;
+let date = document.date.value;
+let otime = document.otime.value;
+let number = document.number.value;
+
+function departure()
+{
+
+}
+
+function arrival()
+{
+
+}
